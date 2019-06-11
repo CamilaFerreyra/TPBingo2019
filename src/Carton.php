eeed4cf8a03b9589576893dee662a181d25ec331
CamilaFerreyra <camila.ferreyra.ips@gmail.com>
2019-06-11 17:10:13 -0300

    Cosas

---
+++ src/Carton.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Bingo;
+
+class Carton implements CartonInterface {
+	public function __construct(array $carton){
+		$this->numeros_carton = $carton;	
+	}
+
+	/**
+   * {@inheritdoc}
+   */
+  public function filas() {
+    return $this->numeros_carton;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function columnas() {
+    $temp = [[], [], [], [], [], [], [], [], []];
+    foreach($this->numeros_carton as $fila){
+      $i = 0;
+      foreach($fila as $celda){
+        $temp[$i++][] = $celda;
+      }
+    }
+    return $temp;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function numerosDelCarton() {
+    $numeros = [];
+    foreach ($this->filas() as $fila) {
+      foreach ($fila as $celda) {
+        if ($celda != 0) {
+          $numeros[] = $celda;
+        }
+      }
+    }
+    return $numeros;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tieneNumero(int $numero) {
+    return in_array($numero, $this->numeros_carton);
+  }
+}
